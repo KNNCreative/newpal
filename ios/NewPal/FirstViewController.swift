3584f955369dc10be4731a5dcc989a4a84158a20
Kien Pham <kien@calolo.com>
2015-11-15 15:04:49 -0800

    z

---
+++ ios/NewPal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  NewPal
+//
+//  Created by Kien Pham on 11/15/15.
+//  Copyright © 2015 KNN Creative. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios/Pods/Target Support Files/Pods-NewPal/Pods-NewPal-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewPal : NSObject
+@end
+@implementation PodsDummy_Pods_NewPal
+@end
